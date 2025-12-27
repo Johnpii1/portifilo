49ab1966aaf84387b11750c320b35b9c4182d731
Johnpii1 <okolichimobi08gmail.com>
2025-12-27 17:33:07 +0100

    update today

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-const btns = document.getElementById("btn");
-const menus = document.getElementById("menu");
-btns.addEventListener("click", () => {
-    menus.classList.toggle("show");
-});
+const menuBtn = document.getElementById("btn");
+const menu = document.querySelector(".menus");
+
+if (menuBtn && menu) {
+  menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+  });
+}
+
+
+
+//dark mood and light mood 
+// const html = document.documentElement;
+// const lightBtn = document.getElementById("lightBtn");
+// const darkBtn = document.getElementById("darkBtn");
+
+// lightBtn.addEventListener("click", () => {
+//   html.classList.remove("dark");
+// });
+
+// darkBtn.addEventListener("click", () => {
+//   html.classList.add("dark");
+// });
+
+
+
+
+
